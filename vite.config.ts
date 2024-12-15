9f7ac74cc4197e222d87340db559a3c394d44b2b
Bill <v.kampouroudis@gmail.com>
2024-12-15 22:32:16 +0200

    Add alias for /src

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [react()],
   css: {
     preprocessorOptions: {
       scss: {
         api: 'modern-compiler',
-        additionalData: `@use '/src/styles/theme/_mantine' as mantine;`,
+        additionalData: `@use '@/styles/theme/_mantine';`,
       },
     },
   },
